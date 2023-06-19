8f104dfbbbc8b1367e3d87c119112990745409ee
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-06-19 21:29:53 +0300

    Create start.sh

---
+++ 5/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+mkdir -p /home/max/cave-of-cmds/cow-house/pile-of-garbage;mkdir -p /home/max/cave-of-cmds/stable;mkdir -p /home/max/cave-of-cmds/garbage-dump;mkdir -p /opt/drak-app/red/yellow/orange;mkdir -p /opt/drak-app/yellow/orange/red;mkdir -p /opt/drak-app/orange/red/yellow;mkdir -p /home/max/tmp/forest/
+mkdir /var/games/animals; mkdir -p /var/zoo/south/animals; mkdir -p /var/lib/docker/animals
+
+echo 'Хорошо, ты умеешь разжимать! ' > /home/max/cave-of-cmds/horse.dat; gzip /home/max/cave-of-cmds/horse.dat;
+
+for i in {1..300};do echo "orange file" > /opt/drak-app/red/yellow/orange/orange.$RANDOM; done;
+for i in {1..300};do echo "red file" > /opt/drak-app/yellow/orange/red/red.$RANDOM; done;
+for i in {1..300};do echo "red file" > /opt/drak-app/orange/red/yellow/yellow.$RANDOM; done;
+
+dd if=/dev/zero of=/home/max/cave-of-cmds/cow-house/pile-of-garbage/garbage.dat count=100024 bs=1024;
+dd if=/dev/zero of=/usr/local/stegosaurus.pet count=100024 bs=1024;
+dd if=/dev/zero of=/etc/stegosaurus.pet count=100024 bs=1024;
+dd if=/dev/zero of=/root/stegosaurus.pet count=100024 bs=1024;
+
+echo "Roskomnadzor: Telegram, give me the keys!" | base64 > /home/max/tmp/forest/elephant-key
+
+echo "Thing to do: take the 'Ansible for beginners course' by ROTORO.CLOUD" > /opt/drak-app/orange/red/yellow/orange-scroll; 
+echo "dragon.dragon85@gmail.com  mysecretpassword000" > /opt/drak-app/red/yellow/orange/red-scroll;
+echo "Dear dragon, the best medicine for your disease will be the urgent '/etc/systemd/system/covid.service' file deletion. Just delete it! Best regards, Dr. Aibolit." > /opt/drak-app/yellow/orange/red/yellow-scroll; 
+echo "" > /etc/systemd/system/covid.service;
+echo "172.20.20.20 madagascar
+
+echo "I am lost..." > /usr/bin/stegosaurus.pet
+
+for i in {1..534};do echo "Wise thought $RANDOM" >> /home/max/endless; done;
