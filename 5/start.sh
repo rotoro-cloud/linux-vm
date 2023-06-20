b8100d8f7475ed6a7c6ab4fbbca1efea58788b85
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-06-20 12:38:41 +0300

    Update start.sh

--- 5/start.sh
+++ 5/start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash 
 
-mkdir -p /home/max/cave-of-cmds/cow-house/pile-of-garbage;mkdir -p /home/max/cave-of-cmds/stable;mkdir -p /home/max/cave-of-cmds/garbage-dump;mkdir -p /opt/drak-app/red/yellow/orange;mkdir -p /opt/drak-app/yellow/orange/red;mkdir -p /opt/drak-app/orange/red/yellow;mkdir -p /home/max/tmp/forest/
+rm -rf /home/max/cave-of-cmds /opt/drak-app/ /usr/local/stegosaurus.pet /etc/stegosaurus.pet /root/stegosaurus.pet /etc/systemd/system/covid.service /usr/bin/stegosaurus.pet /home/max/endless
+
+mkdir -p /home/max/cave-of-cmds/cow-house/pile-of-garbage;
+mkdir -p /home/max/cave-of-cmds/stable;
+mkdir -p /home/max/cave-of-cmds/garbage-dump;
+mkdir -p /opt/drak-app/red/yellow/orange;mkdir -p /opt/drak-app/yellow/orange/red;mkdir -p /opt/drak-app/orange/red/yellow;
+mkdir -p /home/max/tmp/forest/
 mkdir /var/games/animals; mkdir -p /var/zoo/south/animals; mkdir -p /var/lib/docker/animals
 
 echo 'Хорошо, ты умеешь разжимать! ' > /home/max/cave-of-cmds/horse.dat; gzip /home/max/cave-of-cmds/horse.dat;
+
 
 for i in {1..300};do echo "orange file" > /opt/drak-app/red/yellow/orange/orange.$RANDOM; done;
 for i in {1..300};do echo "red file" > /opt/drak-app/yellow/orange/red/red.$RANDOM; done;
@@ -20,7 +27,7 @@ echo "Thing to do: take the 'Ansible for beginners course' by ROTORO.CLOUD" > /o
 echo "dragon.dragon85@gmail.com  mysecretpassword000" > /opt/drak-app/red/yellow/orange/red-scroll;
 echo "Dear dragon, the best medicine for your disease will be the urgent '/etc/systemd/system/covid.service' file deletion. Just delete it! Best regards, Dr. Aibolit." > /opt/drak-app/yellow/orange/red/yellow-scroll; 
 echo "" > /etc/systemd/system/covid.service;
-echo "172.20.20.20 madagascar
+echo "172.20.20.20 madagascar" > /etc/turtlehosts;
 
 echo "I am lost..." > /usr/bin/stegosaurus.pet
 
