41f2b7cf8c3148cac98abad48e179fd7e88c47db
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-06-20 18:01:41 +0300

    Update task6.sh

--- 6/task6.sh
+++ 6/task6.sh
@@ -1,1 +1,13 @@
+#!/bin/bash 
 
+groupadd -r pollme;  useradd -r -d /home/max -s /bin/bash -g pollme pollme
+mkdir -p /home/max/games
+chown max:max /home/max/games
+
+touch  /home/max/games/residentevil
+chown max:max /home/max/games/residentevil
+chmod 777 /home/max/games/residentevil
+
+mkdir -p /var/log/packer/sys
+touch  /var/log/packer/sys/ok-its-me
+chmod 020 /var/log/packer/sys/ok-its-me
