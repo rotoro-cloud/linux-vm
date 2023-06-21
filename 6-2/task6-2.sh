67ef38be71ffa7eb81b9b354a4057c913b4baf95
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-06-21 09:54:35 +0300

    Update task6-2.sh

--- 6-2/task6-2.sh
+++ 6-2/task6-2.sh
@@ -7,6 +7,8 @@ echo 172.20.236.10 str01 >> /etc/hosts
 echo 172.20.235.10 db01 >> /etc/hosts
 
 
+myip=$(ip a | grep "scope global" | grep enp0 | awk '{print $2}' | awk -F/ '{print $1}') && echo $myip ws01 >> /etc/hosts
+
 gip=$(nslookup google.com | grep Address | grep -vi "\#" | awk '{print $2}' | grep ^[0-9][0-9]) &&  echo $gip google.com >> /etc/hosts
 
 archive_ip=$(nslookup archive.ubuntu.com | grep Address | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -vi "\#" | head -1 |awk '{print $2}') && echo $archive_ip archive.ubuntu.com >> /etc/hosts
