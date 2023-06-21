aa2d24f5698c74315799f49f7bd2e9bb375b3420
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-06-21 09:47:39 +0300

    Update task6-2.sh

--- 6-2/task6-2.sh
+++ 6-2/task6-2.sh
@@ -1,5 +1,5 @@
 apt update
-apt install -y dnsutils net-tools
+apt install -y dnsutils net-tools curl
 
 echo 10.0.2.20 jump_host >> /etc/hosts
 echo 172.20.236.101 app01 >> /etc/hosts
@@ -12,3 +12,9 @@ gip=$(nslookup google.com | grep Address | grep -vi "\#" | awk '{print $2}' | gr
 archive_ip=$(nslookup archive.ubuntu.com | grep Address | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -vi "\#" | head -1 |awk '{print $2}') && echo $archive_ip archive.ubuntu.com >> /etc/hosts
 
 archive_ip2=$(nslookup archive.canonical.com | grep Address | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -vi "\#" | head -1 |awk '{print $2}') && echo $archive_ip2 archive.canonical.com >> /etc/hosts
+
+archive_ip3=$(nslookup security.ubuntu.com | grep Address | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -vi "\#" | head -1 |awk '{print $2}') && echo $archive_ip3 security.ubuntu.com >> /etc/hosts
+
+archive_ip4=$(nslookup us.archive.ubuntu.com | grep Address | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -vi "\#" | head -1 |awk '{print $2}') && echo $archive_ip4 us.archive.ubuntu.com >> /etc/hosts
+
+archive_ip5=$(nslookup banjo.canonical.com | grep Address | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -vi "\#" | head -1 |awk '{print $2}') && echo $archive_ip5 banjo.canonical.com >> /etc/hosts
