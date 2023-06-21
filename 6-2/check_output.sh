f57e553c67b649fde437bfe76632a9406b3086fa
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-06-21 10:52:13 +0300

    Create check_output.sh

---
+++ 6-2/check_output.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+PURPLE='\033[1;36m'
+NC='\033[0m'
+
+iptables -L OUTPUT | grep db01 | grep dpt:mysql || echo -e "${RED}Проблема в правиле для базы с ws01! ${NC}"
+iptables -L OUTPUT | grep str01 | grep -w dpt:http$ || echo -e "${RED}Проблема в правиле для репо с ws01! ${NC}"
+
+iptables -L OUTPUT | tail -2 | grep DROP | grep -w dpt:http$ || echo -e "${RED}Проблема в общем правиле для http с ws01! ${NC}"
+iptables -L OUTPUT | tail -2 | grep -w dpt:https | grep DROP || echo -e "${RED}ППроблема в общем правиле для https с ws01! ${NC}"
