03a56bd96041f74124ebbf9cb380f32d8b1ea8de
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-06-21 21:55:38 +0300

    Update setup_app01.sh

--- 7/setup_app01.sh
+++ 7/setup_app01.sh
@@ -94,3 +94,4 @@ result_msg "$?" "hostname patched"
 ssh -i /home/max/.ssh/id_rsa -o StrictHostKeyChecking=no max@app01 "echo supeRbison | sudo -S bash -c \"echo \\\"$my_ip ws01\\\" >> /etc/hosts\""
 result_msg "$?" "hosts pathced"
 
+echo "$my_ip ws01" >> /etc/hosts
