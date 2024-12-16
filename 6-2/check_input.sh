53012ea0c2209f95aa612e1671a0a8a9218de585
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2024-12-16 09:19:35 +0300

    Update check_input.sh

--- 6-2/check_input.sh
+++ 6-2/check_input.sh
@@ -9,7 +9,7 @@ NC='\033[0m'
 iptables -L INPUT | grep jump_host | grep -i ssh || echo -e "${RED}Проблема в правиле SSH с jump_host! ${NC}"
 iptables -L INPUT | grep app01 | grep -i http || echo -e "${RED}Проблема в правиле HTTP с app01! ${NC}"
 
-iptables -L INPUT -n | tail -1 | awk '{print $5}'| grep '0.0.0.0/0' && iptables -L INPUT -n | tail -1 | awk '{print $4}'| grep '0.0.0.0/0' && iptables -L INPUT -n | tail -1 | awk '{print $2}'| grep all
+iptables -L INPUT -n | tail -1 | awk '{print $5}'| grep '0.0.0.0/0' && iptables -L INPUT -n | tail -1 | awk '{print $4}'| grep '0.0.0.0/0' && iptables -L INPUT -n | tail -1 | awk '{print $2}'| grep "all\|0"
 
 if [ $? -ne 0 ]; then
     echo -e "${RED} Проблема в дефолтном правиле! ${NC}"
